b7bf1cc8dd1631130108dd44cc6b3f2a5d091d1d
VJ Patel <vj@vjpatel.me>
2020-05-25 19:24:18 +0100

    add logging

--- deployments/infrastructure/netlify-cms-oauth.tf
+++ deployments/infrastructure/netlify-cms-oauth.tf
@@ -18,6 +18,39 @@ resource "aws_iam_role" "netlifycmsoauth" {
 EOF
 }
 
+resource "aws_cloudwatch_log_group" "netlifycmsoauth" {
+  name              = "/aws/lambda/${aws_lambda_function.netlifycmsoauth.function_name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "netlifycmsoauth_logging" {
+  name        = "netlifycmsoauth_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.netlifycmsoauth.name
+  policy_arn = aws_iam_policy.netlifycmsoauth_logging.arn
+}
+
 resource "aws_lambda_function" "netlifycmsoauth" {
   function_name = "netlifycmsoauth-${local.safe_base_dns}"
   role          = aws_iam_role.netlifycmsoauth.arn
