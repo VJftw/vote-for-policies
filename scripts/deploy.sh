1edb36df9d326892635a1988bce7c64fe0c0edba
VJ Patel <vj@vjpatel.me>
2020-06-10 23:31:34 +0100

    add serverless dev deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,14 @@ if [ -z "${env}" ]; then
   exit 2
 fi
 
+config="serverless.yaml"
 export DEPLOY_DOMAIN="${env}.survey.vfp.vjpatel.me"
 if [ "${env}" == "production" ]; then
   export DEPLOY_DOMAIN="${baseDomain}"
+elif [ "${env}" == "staging" ]; then
+  export DEPLOY_DOMAIN="${env}.survey.vfp.vjpatel.me"
+else
+  config="serverless.dev.yaml"
 fi
 
 # 1. Pull state from S3 if it exists
@@ -20,7 +25,7 @@ aws s3 cp "${stateBucket}/${env}.tar.gz" . && tar -xzf "${env}.tar.gz" || echo "
 echo "-> Deploying to ${DEPLOY_DOMAIN}"
 
 # 2. Deploy
-npx serverless --debug
+npx serverless --debug --config "${config}"
 
 # 3. Push state to S3
 tar -czf "${env}.tar.gz" ".serverless" && aws s3 cp "${env}.tar.gz" "${stateBucket}/${env}.tar.gz"
